4b421a3bd8c7f37390f54277f275521e3da6d3c1
Hoang Tuan Anh <hoangtuananh55898@gmail.com>
2026-07-08 03:16:57 +0700

    Nhập lời nhắn giải thích bạn vừa code xong cái gì

--- src/main/App.java
+++ src/main/App.java
@@ -1,15 +1,8 @@
 package main;
 
 import java.util.Scanner;
-
 import service.*;
-
-import view.ComboView;
-import view.EmployeeView;
-import view.FinanceView;
-import view.OrderView;
-import view.ProductView;
-import view.ReservationView;
+import view.*;
 
 public class App {
 
@@ -17,7 +10,6 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
-        //================ SERVICE =================
         AccountService accountService = new AccountService();
         EmployeeService employeeService = new EmployeeService();
         ProductService productService = new ProductService();
@@ -26,112 +18,141 @@ public class App {
         ReservationService reservationService = new ReservationService();
         FinanceService financeService = new FinanceService();
 
-        while (true) {
+        int choose = -1;
 
-            System.out.println("\n======================================");
-            System.out.println("      TEA HOUSE MANAGEMENT SYSTEM");
-            System.out.println("======================================");
-            System.out.println("1. Login");
+        do {
+
+            System.out.println("\n=================================");
+            System.out.println(" TEA HOUSE MANAGEMENT SYSTEM");
+            System.out.println("=================================");
+            System.out.println("1. Admin");
+            System.out.println("2. Order");
             System.out.println("0. Exit");
             System.out.print("Choose: ");
-
-            int choose;
 
             try {
                 choose = Integer.parseInt(sc.nextLine());
             } catch (Exception e) {
-                System.out.println("Invalid choice!");
+                System.out.println("Invalid Choice!");
                 continue;
             }
 
-            if (choose == 0) {
-                System.out.println("Good Bye!");
-                break;
+            switch (choose) {
+
+                //================ ADMIN =================
+                case 1:
+
+                    System.out.println("\n========== LOGIN ==========");
+
+                    System.out.print("Username: ");
+                    String username = sc.nextLine();
+
+                    System.out.print("Password: ");
+                    String password = sc.nextLine();
+
+                    if (!accountService.login(username, password)) {
+                        System.out.println("Login Failed!");
+                        break;
+                    }
+
+                    System.out.println("Login Successfully!");
+
+                    adminMenu(sc,
+                            employeeService,
+                            productService,
+                            comboService,
+                            reservationService,
+                            orderService,
+                            financeService);
+
+                    break;
+
+                //================ ORDER =================
+                case 2:
+
+                    System.out.println("\n====== ORDER MODE ======");
+                    OrderView.orderMenu(sc, orderService);
+
+                    break;
+
+                case 0:
+                    System.out.println("Good Bye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice!");
             }
 
-            if (choose != 1) {
-                System.out.println("Invalid choice!");
-                continue;
-            }
-
-            //---------------- LOGIN ----------------
-
-            System.out.print("Username: ");
-            String username = sc.nextLine();
-
-            System.out.print("Password: ");
-            String password = sc.nextLine();
-
-            if (!accountService.login(username, password)) {
-                System.out.println("Login Failed!");
-                continue;
-            }
-
-            System.out.println("Login Successfully!");
-
-            //---------------- MAIN MENU ----------------
-
-            int menu;
-
-            do {
-
-                System.out.println("\n============= MAIN MENU =============");
-                System.out.println("1. Employee Management");
-                System.out.println("2. Product Management");
-                System.out.println("3. Combo Management");
-                System.out.println("4. Order Management");
-                System.out.println("5. Reservation Management");
-                System.out.println("6. Finance Management");
-                System.out.println("0. Logout");
-                System.out.println("=====================================");
-                System.out.print("Choose: ");
-
-                try {
-                    menu = Integer.parseInt(sc.nextLine());
-                } catch (Exception e) {
-                    System.out.println("Invalid choice!");
-                    menu = -1;
-                    continue;
-                }
-
-                switch (menu) {
-
-                    case 1:
-                        new EmployeeView(employeeService).run();
-                        break;
-
-                    case 2:
-                        new ProductView(productService).run();
-                        break;
-
-                    case 3:
-                        ComboView.comboMenu(sc, comboService);
-                        break;
-
-                    case 4:
-                        OrderView.orderMenu(sc, orderService);
-                        break;
-
-                    case 5:
-                        new ReservationView(reservationService).menu();
-                        break;
-
-                    case 6:
-                        FinanceView.displayFinancialReport(sc, financeService);
-                        break;
-
-                    case 0:
-                        System.out.println("Logout Successfully!");
-                        break;
-
-                    default:
-                        System.out.println("Invalid choice!");
-                }
-
-            } while (menu != 0);
-
-        }
+        } while (choose != 0);
 
         sc.close();
+    }
+
+    //=========================================================
+    public static void adminMenu(
+            Scanner sc,
+            EmployeeService employeeService,
+            ProductService productService,
+            ComboService comboService,
+            ReservationService reservationService,
+            OrderService orderService,
+            FinanceService financeService) {
+
+        int menu = -1;
+
+        do {
+
+            System.out.println("\n============= ADMIN MENU =============");
+            System.out.println("1. Employee Management");
+            System.out.println("2. Product Management");
+            System.out.println("3. Combo Management");
+            System.out.println("4. Reservation Management");
+            System.out.println("5. Order Management");
+            System.out.println("6. Finance Management");
+            System.out.println("0. Logout");
+            System.out.print("Choose: ");
+
+            try {
+                menu = Integer.parseInt(sc.nextLine());
+            } catch (Exception e) {
+                System.out.println("Invalid Choice!");
+                continue;
+            }
+
+            switch (menu) {
+
+                case 1:
+                    new EmployeeView(employeeService).run();
+                    break;
+
+                case 2:
+                    new ProductView(productService).run();
+                    break;
+
+                case 3:
+                    ComboView.comboMenu(sc, comboService);
+                    break;
+
+                case 4:
+                    new ReservationView(reservationService).menu();
+                    break;
+
+                case 5:
+                    OrderView.orderMenu(sc, orderService);
+                    break;
+
+                case 6:
+                    FinanceView.displayFinancialReport(sc, financeService);
+                    break;
+
+                case 0:
+                    System.out.println("Logout Successfully!");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice!");
+            }
+
+        } while (menu != 0);
     }
 }
